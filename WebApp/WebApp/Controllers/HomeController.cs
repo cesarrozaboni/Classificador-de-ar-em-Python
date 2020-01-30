864e3d175fed2b835b21fdf519259941f4153e12
Cesar Rozaboni <cesarrozaboni@gmail.com>
2020-01-29 21:59:38 -0300

    Desenvolvimento

---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.ViewModel;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public JsonResult DadosProduto(Produto produto)
+        {
+            int valor;
+
+            if(!int.TryParse(produto.Valor, out valor))
+            {
+                return Json("Valor do produto esta incorreto", JsonRequestBehavior.AllowGet);
+            }
+
+            if (!int.TryParse(produto.ValorEntrada, out valor))
+            {
+                return Json("Valor de entrada incorreto", JsonRequestBehavior.AllowGet);
+            }
+
+            if (!int.TryParse(produto.QtdParcelas, out valor))
+            {
+                return Json("Quantidade de parcelas esta incorreto", JsonRequestBehavior.AllowGet);
+            }
+
+            var lClt = new WebClient();
+
+            string lURL = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados";
+            
+            byte[] lDataBuffer = lClt.DownloadData(lURL);
+
+            string lOut = Encoding.ASCII.GetString(lDataBuffer);
+
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApp/WebApp/ViewModel/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.ViewModel
+{
+    [Serializable]
+    public class Produto
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+        public string ValorEntrada { get; set; }
+        public string QtdParcelas { get; set; }
+    }
+
+
+}
